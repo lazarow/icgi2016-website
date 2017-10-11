749151a1012a82af2810bee915b9675e7d4a9281
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2017-10-11 20:37:15 +0200

    configuration.php

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ $app = new \Slim\App([
 	'displayErrorDetails' => false,
 	
 ]);
+if (file_exists(__DIR__ . '/protected/application/configuration.php')) {
+	require __DIR__ . '/protected/application/configuration.php';
+}
 require __DIR__ . '/protected/application/dependencies.php';
 require __DIR__ . '/protected/application/middleware.php';
 require __DIR__ . '/protected/application/routes.php';
