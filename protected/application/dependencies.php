11c500f8329fc6738d71c9cd2168448f087cf37e
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2017-10-11 22:34:20 +0200

    Add theme and content

--- protected/application/dependencies.php
+++ protected/application/dependencies.php
@@ -3,6 +3,7 @@ $app->getContainer()['view'] = function ($container) {
     $view = new \Slim\Views\Twig($container->get('settings')['twigTemplatesDir'], [
 		'cache' => $container->get('settings')['twigCacheDir']
     ]);
+	$view->offsetSet('base_url', $container['request']->getUri()->getBaseUrl());
     $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
     $view->addExtension(new \Slim\Views\TwigExtension($container['router'], $basePath));
     return $view;
